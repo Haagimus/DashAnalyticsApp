34e77cf35e2fa1503cfc8ea9d451576237db125b
GHaag <gary.haag@l3harris.com>
2019-10-24 09:37:26 -0500

    split employee database stuff into separate file

--- Resource Demand.py
+++ Resource Demand.py
@@ -1,38 +1,9 @@
-from sqlalchemy.connectors import pyodbc
-from sqlalchemy.inspection import inspect
-import pyodbc
-import pandas
-import numpy
 import dash
 import dash_table as dt
 import dash_core_components as dcc
 import dash_html_components as html
 
-EmpTblSQL = 'SELECT * FROM dbo.Employees'
-FinTblSql = 'SELECT * FROM dbo.Finance_Functions'
-HRTblSQL = 'SELECT * FROM dbo.HR_Functions'
-
-conn = pyodbc.connect(
-    'DRIVER={SQL Server};SERVER=FRXSV-DAUPHIN;DATABASE=FRXResourceDemand')
-
-empDF = pandas.read_sql(EmpTblSQL, conn)
-finDF = pandas.read_sql(FinTblSql, conn)
-hrDF = pandas.read_sql(HRTblSQL, conn)
-
-count = {}
-idx = 0
-for title in finDF.values:
-    count.update({title[0]: 0})
-    for label, row in empDF.iterrows():
-        if row['Function_Finance'] == title and row['Date_End'] == None:
-            count[title[0]] += 1
-    idx += 1
-
-total = 0
-for (key, value) in count.items():
-    print(key, '::', value)
-    total += value
-print('{0} total staff'.format(total))
+import employees
 
 app = dash.Dash(__name__)
 
@@ -47,9 +18,9 @@ app.layout = html.Div([
         },
         id='Employees',
         columns=[
-            {'name': i, 'id': i, 'selectable': True} for i in empDF.columns
+            {'name': i, 'id': i, 'selectable': True} for i in employees.empDF.columns
         ],
-        data=empDF.to_dict('records'),
+        data=employees.empDF.to_dict('records'),
         editable=True,
         filter_action='native',
         sort_action='native',
---
+++ employees.py
@@ -0,0 +1,30 @@
+from sqlalchemy.connectors import pyodbc
+from sqlalchemy.inspection import inspect
+import pyodbc
+import pandas
+
+EmpTblSQL = 'SELECT * FROM dbo.Employees'
+FinTblSql = 'SELECT * FROM dbo.Finance_Functions'
+HRTblSQL = 'SELECT * FROM dbo.HR_Functions'
+
+conn = pyodbc.connect(
+    'DRIVER={SQL Server};SERVER=FRXSV-DAUPHIN;DATABASE=FRXResourceDemand')
+
+empDF = pandas.read_sql(EmpTblSQL, conn)
+finDF = pandas.read_sql(FinTblSql, conn)
+hrDF = pandas.read_sql(HRTblSQL, conn)
+
+count = {}
+idx = 0
+for title in finDF.values:
+    count.update({title[0]: 0})
+    for label, row in empDF.iterrows():
+        if row['Function_Finance'] == title and row['Date_End'] == None:
+            count[title[0]] += 1
+    idx += 1
+
+total = 0
+for (key, value) in count.items():
+    print(key, '::', value)
+    total += value
+print('{0} total staff'.format(total))
