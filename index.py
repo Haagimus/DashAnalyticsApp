74a059c884391f762d9b5cdec2d58e9003ce6ab7
GHaag <gary.haag@l3harris.com>
2019-10-25 12:11:58 -0500

    navbar work

--- index.py
+++ index.py
@@ -4,15 +4,26 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 
+import navbar
+
 app = dash.Dash(__name__)
+nav = navbar.Navbar()
 
 app.layout = html.Div([
-    html.Ul([
-        html.Li([dcc.Link('Home', href='/')]),
-        html.Li([dcc.Link('Employee', href='/')]),
-        html.Li([dcc.Link('Programs', href='/')])
-    ])
+    # represents the URL bar, doesn't render anythin
+    dcc.Location(id='url', refresh=False),
+    nav,
+    html.Div(id='page-content')
 ])
+
+
+@app.callback(dash.dependencies.Output('page-content', 'children'),
+              [dash.dependencies.Input('url', 'pathname')])
+def display_page(pathname):
+    return html.Div([
+        nav,
+        html.H3('You are on page {}'.format(pathname))
+    ])
 
 
 if __name__ == '__main__':
--- navbar.py
+++ navbar.py
@@ -1,26 +1,13 @@
 # this file will create the navabar for the top of pages
-import dash_bootstrap_components as dbc
+import dash_core_components as dcc
+import dash_html_components as html
 
 
 def Navbar():
-    navbar = dbc.NavbarSimple(
-        children=[
-            dbc.NavItem(dbc.NavLink("Page 1", href="#")),
-            dbc.DropdownMenu(
-                children=[
-                    dbc.DropdownMenuItem("More pages", header=True),
-                    dbc.DropdownMenuItem("Page 2", href="#"),
-                    dbc.DropdownMenuItem("Page 3", href="#"),
-                ],
-                nav=True,
-                in_navbar=True,
-                label="More",
-            ),
-        ],
-        brand="NavbarSimple",
-        brand_href="#",
-        color="primary",
-        dark=True,
-    )
+    navbar = html.Ul([
+        html.Li([dcc.Link('Home', href='/')]),
+        html.Li([dcc.Link('Employee', href='/employees.py')]),
+        html.Li([dcc.Link('Programs', href='/')])
+    ])
 
     return navbar
