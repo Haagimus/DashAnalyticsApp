decdf68c920d572218a8c95293000fea69225ad3
GHaag <gary.haag@l3harris.com>
2019-11-13 15:15:37 -0600

    limited email send to on, needs to be reimplemented in a different manner, added sender email to the email list so they receive copy or message

--- pages/home.py
+++ pages/home.py
@@ -10,8 +10,12 @@ import smtplib
 form = dbc.Form(children=[
     html.H1("User Submission Form", style={'color': 'white'}),
     dbc.FormGroup([
-        dbc.Input(id='email', type="email", placeholder="Enter your email address",
-                  style={'width': '400px', 'marginTop': '3%'}),
+        dcc.Input(id='email',
+                  type="email",
+                  placeholder="Enter your email address",
+                  required=True,
+                  style={'width': '400px', 'marginTop': '3%'}
+                  )
     ],
         className="mr-2",
     ),
@@ -29,7 +33,8 @@ form = dbc.Form(children=[
     ),
     dbc.FormGroup([
         dbc.Textarea(id="comment", className="mr-2", bs_size="lg",
-                     placeholder="Enter comments/suggestions", style={'width': '400px', 'height': '200px'},
+                     placeholder="Enter comments/suggestions",
+                     style={'width': '400px', 'height': '200px'},
                      value='')],
                   className="mr-2"),
     dbc.Button("Submit", id='submit', n_clicks=0),
@@ -49,23 +54,21 @@ def Home():
 
 def send_mail(orig_from, subject, body, msgType):
     port = 25
-    # uncomment below for production
-    # smtp_server = "frxsv-globemaster"
-    # uncomment for local testing
-    smtp_server = "relay.l3t.com"
+    smtp_server = "frxsv-globemaster"
     sender_email = "FRX.DevOps@L3Harris.com"
     user = orig_from
-    receiver_email = "stephen.french@l3harris.com;gary.haag@l3harris.com"
+    receiver_email = ['stephen.french@l3harris.com',
+                      'gary.haag@l3harris.com', orig_from]
     message = """"\
     Subject: {0} has submitted a {1}
 
 
     Submission: {2}. """.format(subject, msgType, body)
-
     try:
         server = smtplib.SMTP(smtp_server, port)
         server.login("FRX.EmailRelay@iss.l3t.com", "N)QQH3hppTrthKQN")
-        server.sendmail(sender_email, receiver_email, message)
+        for email in receiver_email:
+            server.sendmail(sender_email, email, message)
         server.quit()
         return True
     except Exception as e:
@@ -79,19 +82,22 @@ def send_mail(orig_from, subject, body, msgType):
                Input('submit', 'n_clicks'),
                Input('email', 'value')])
 def send_submission(msgType_value, comment_value, n_clicks, email_value):
+    count = 0
     if n_clicks > 0 and n_clicks < 2:
-        if msgType_value == "1":
-            msgType = "Bug Report"
-        elif msgType_value == "2":
-            msgType = "Feature Request"
-        elif msgType_value == "3":
-            msgType = "Request Admin"
-        subject = "A new " + msgType + " was submtited."
-        body = comment_value
-        if send_mail(email_value, subject, body, msgType):
-            return "Message sent successfully"
-        else:
-            return "Message unable to send. Try resetting form"
+        while count < 1:
+            if msgType_value == "1":
+                msgType = "Bug Report"
+            elif msgType_value == "2":
+                msgType = "Feature Request"
+            elif msgType_value == "3":
+                msgType = "Request Admin"
+            subject = "A new " + msgType + " was submtited."
+            body = comment_value
+            if send_mail(email_value, subject, body, msgType):
+                return "Message sent successfully"
+            else:
+                return "Message unable to send. Try resetting form"
+            count = 1
 
 
 @app.callback(Output('submit', 'n_clicks'),
