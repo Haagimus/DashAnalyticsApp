88618ed08cea08e0b0295ef489e2c188e66bda7c
GHaag <gary.haag@l3harris.com>
2019-11-14 16:06:41 -0600

    created the registation modal with callbacks to open and close

--- assets/navbar.py
+++ assets/navbar.py
@@ -4,6 +4,7 @@ import dash_bootstrap_components as dbc
 from dash.dependencies import Output, Input, State
 from server import app
 from .callbacks import HomeLink, EmpLink, PgmLink, CapLink
+from assets.SQL import GetTable
 
 
 # When new pages are added, update list with href to location
@@ -22,34 +23,78 @@ def Navbar():
                 [dcc.Link('Capacity', id='capLink', href='/capacity')]),
             html.Ul(id='navRight', children=[
                 html.Li(
-                    [html.Button('Register', id='register', n_clicks=0)]),
+                    [html.Button('Register', id='registerOpen', n_clicks=0)]),
                 html.Li(
-                    [html.Button('Login', id='login', n_clicks=0)])
+                    [html.Button('Login', id='loginOpen', n_clicks=0)])
             ])
         ]),
-        html.Div([
-            html.Div([
-                html.P(children=[''], id='loginMessage'),
-                html.H3('Please log in:', id='loginHead'),
-                html.P(children=['Username: ',
-                                 dcc.Input(type='text',
-                                           id='username',
-                                           className='required',
-                                           required=True,
-                                           placeholder='username')]),
-                html.P(children=['Password: ',
-                                 dcc.Input(type='password',
-                                           id='password',
-                                           className='required',
-                                           required=True,
-                                           placeholder='password')]),
-                html.Button('Login', id='login-modal', n_clicks=0),
-                html.Button('Close', id='close', n_clicks=0)
-            ],
-                className='modal-content')
-        ],
-            id='myModal',
-            className='modal')
+        login_modal,
+        registation_modal
     ])
 
     return navbar
+
+
+# The login modal page layout
+login_modal = html.Div([
+    html.Div([
+        html.P(children=[''], id='loginMessage'),
+        html.H3('Please log in:', id='loginHead'),
+        html.P(children=['Username: ',
+                         dcc.Input(type='text',
+                                   id='loginUsername',
+                                   className='required username',
+                                   required=True,
+                                   placeholder='username')]),
+        html.P(children=['Password: ',
+                         dcc.Input(type='password',
+                                   id='loginPassword',
+                                   className='required password',
+                                   required=True,
+                                   placeholder='password')]),
+        html.Button('Login', id='loginSubmit', n_clicks=0),
+        html.Button('Close', id='loginClose', n_clicks=0)
+    ],
+        className='modal-content')
+],
+    id='loginView',
+    className='modal')
+
+empNumList = GetTable('EmployeeNumbers')
+
+# The registration modal page layout
+registation_modal = html.Div([
+    html.Div([
+        html.H1('Register new user:'),
+        html.Form([
+            html.P(children=['Username: ',
+                             dcc.Input(type='text',
+                                       id='registerUsername',
+                                       className='required username',
+                                       required=True,
+                                       placeholder='username')]),
+            html.P(children=['Employee #: ',
+                             dcc.Dropdown(id='emp-num-drowpdown',
+                                          options=[{'label': i, 'value': i}
+                                                   for i in empNumList.values[0]],
+                                          multi=False)]),
+            html.P(children=['Password: ',
+                             dcc.Input(type='password',
+                                       id='registerPassword',
+                                       className='required password',
+                                       required=True,
+                                       placeholder='password')]),
+            html.P(children=['Re-enter Password',
+                             dcc.Input(type='password',
+                                       id='registerPassword2',
+                                       required=True,
+                                       className='required password',
+                                       placeholder='Re-enter password')]),
+            html.Button('Register', id='registerSubmit', n_clicks=0),
+            html.Button('Close', id='registerClose', n_clicks=0)
+        ])
+    ],
+        className='register-modal')
+],
+    id='registerModal',
+    className='modal')
