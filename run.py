9c578007d98e4cbc0d53c4ca02ba443dd8022993
GHaag <gary.haag@l3harris.com>
2019-12-05 15:09:43 -0600

    file structure was rearranged after latest dev branch merge, fixed it

--- run.py
+++ run.py
@@ -1,9 +1,19 @@
-from app import app
-import sys
+# from app import app
+# import sys
+import create_app
+import os
 
-# Main application loop
+
+config_name = os.getenv('FLASK_CONFIG')
+app = create_app(config_name)
+
+
 if __name__ == '__main__':
-    if sys.argv[1] == 'debug':
-        app.run_server(debug=True, host='127.0.0.1', port='8080')
-    else:
-        app.run_server(debug=False, host='166.20.109.188', port='8080')
+    app.run()
+
+# # Main application loop
+# if __name__ == '__main__':
+#     if sys.argv[1] == 'debug':
+#         app.run_server(debug=True, host='127.0.0.1', port='8080')
+#     else:
+#         app.run_server(debug=False, host='166.20.109.188', port='8080')
--- server.py
+++ server.py
@@ -1,8 +1,10 @@
 from flask import Flask
-from dash import Dash
 from flask_sqlalchemy import SQLAlchemy
+from dash import Dash
 
-server = Flask('Resource and Analysis Website')
-app = Dash(server=server)
-db = SQLAlchemy()
+__name__ = 'Resource and Analysis Website'
+
+server = Flask(__name__)
+app = Dash(__name__, server=server, url_base_pathname='/')
 app.config.suppress_callback_exceptions = True
+db = SQLAlchemy(server)
