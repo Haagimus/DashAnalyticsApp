e3b38d245a1c7606b869a8d5895590eb989a4fd3
GHaag <gary.haag@l3harris.com>
2019-12-18 12:55:17 -0600

    update launch arguments to correct an issue

--- run.py
+++ run.py
@@ -4,7 +4,8 @@ from app import app
 
 # Main application loop
 if __name__ == '__main__':
-    if sys.argv[1] == 'debug':
-        app.run_server(debug=True, host='127.0.0.1', port='8080')
+    if len(sys.argv) == 2:
+        if sys.argv[1] == 'debug':
+            app.run_server(debug=True, host='127.0.0.1', port='8080')
     else:
         app.run_server(debug=False, host='166.20.109.188', port='8080')
